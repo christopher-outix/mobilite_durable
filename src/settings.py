e6e981bcf6da6f64f4b0d579d2e8104687992330
Christopher <christopher.forzy@ecomail.fr>
2026-05-19 18:09:08 +0200

    Nouvelle modification de la valeur, en utilisant les fonctionnalités du module pathlib

--- src/settings.py
+++ src/settings.py
@@ -2,7 +2,7 @@ import logging
 import typing
 from pathlib import Path
 
-DATA_FOLDER: Path = Path.cwd() / ".." / ".." / "src" / "data"
+DATA_FOLDER: Path = Path(__file__).parent.parent / "src" / "data"
 LOGGER_LEVEL: typing.Final = logging.INFO
 EPSG_WEB_MERCATOR: str = "EPSG:3857"
 EPSG_WGS84: str = "EPSG:4326"
